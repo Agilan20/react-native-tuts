adc0a9bd5714ebf95981b4146ce5e453611bf1cd
Agilan <agilan20042003@gmail.com>
2023-03-23 21:02:45 +0530

    first-commit

---
+++ components/header.js
@@ -0,0 +1,11 @@
+import React,{ useState } from "react";
+
+import {View, Text, TextInput} from "react-native"
+
+export default function Header(){
+    return(
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
